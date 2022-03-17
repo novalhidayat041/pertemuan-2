0781d13f1be92c5f302f6a9ccf799403f84c3117
Noval <mnovalhidayat298@gmail.com>
2022-03-17 10:58:30 +0700

    Contoh Penggunaan Collection

--- src/main/kotlin/6.Collections.kt
+++ src/main/kotlin/6.Collections.kt
@@ -1,3 +1,5 @@
+//Koleksi biasanya berisi sejumlah objek (mungkin berjumlah 0) yang memiliki tipe yang sama.
+//Objek dalam sebuah koleksi dipanggil dengan elemen atau item.
 //Contoh Collections
 
 fun printAll(strings: Collection<String>) {
