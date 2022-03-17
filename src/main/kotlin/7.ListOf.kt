525dcf03cc634b34e2b1f4bcffa364385603fc69
Noval <mnovalhidayat298@gmail.com>
2022-03-17 11:16:27 +0700

    Contoh Penggunaan listOf

---
+++ src/main/kotlin/7.ListOf.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>) {
+    val numbers = listOf("one", "two", "three", "four")
+    println("Number of elements: ${numbers.size}")
+    println("Third elements: ${numbers.get(2)}")
+    println("Fourth element: ${numbers[3]}")
+    println("index of element \"two\" ${numbers.indexOf("two")}")
+
+    //Daftar elemen (termasuk nol) dapat diduplikasi
+    val bob = Person("Bob", 31)
+    val people = listOf<Person>(Person("Adam",20), bob, bob)
+    val people2 = listOf<Person>(Person("Adam", 20), Person("Bob", 31), bob)
+    println(people == people2)
+    bob.age = 32
+    println(people == people2)
+}
+
+class Person(var name: String, var age: Int) {}
