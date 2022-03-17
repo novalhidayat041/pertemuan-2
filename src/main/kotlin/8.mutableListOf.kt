14fb6710da3b99711e6471d0452f626481ec4797
Noval <mnovalhidayat298@gmail.com>
2022-03-17 11:31:09 +0700

    Contoh Penggunaan MuttablelistOf

---
+++ src/main/kotlin/8.mutableListOf.kt
@@ -0,0 +1,14 @@
+fun main(array: Array<String>) {
+    //MutableList adalah Daftar dengan operasi penulisan khusus daftar
+    val numbers = mutableListOf(1, 2, 3, 4)
+    numbers.add(5)
+    numbers.removeAt(1)
+    numbers[0] = 0
+    numbers.shuffle()
+    println(numbers)
+
+    val numbersStr = mutableListOf("one", "two", "three", "four")
+    numbersStr.add("five") //ini oke
+    //numbersStr = mutableListOf("six", "seven") // compilation error
+    println(numbersStr)
+}
