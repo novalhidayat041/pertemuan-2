d60ede93ae510839fc982ca2532a06d366290d75
Noval <mnovalhidayat298@gmail.com>
2022-03-18 21:08:49 +0700

    Contoh Penggunaan Percabangan

--- src/main/kotlin/15.When.kt
+++ src/main/kotlin/15.When.kt
@@ -13,6 +13,6 @@ fun main(args: Array<String>) {
     when (x) {
         1,2 -> println("Value of X either 1, 2")
         else -> { // Note the block
-        println("x is neither 1 nor 2") }
+        println("x .is neither 1 nor 2") }
     }
 }
